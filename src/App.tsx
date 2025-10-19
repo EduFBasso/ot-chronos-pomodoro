a4aca398b31cfc064da9956ab4547658cc792a1c
Eduardo Basso <edu_fabric@outlook.com>
2025-10-19 11:14:06 -0300

    A15: files .module.css -> sobreescrição de estilos, padrões, imports...

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Heading } from './components/Heading';
 
 export function App() {
   console.log('Oi');
 
   return (
     <>
-      <h1>Olá mundo!</h1>
+      <Heading />
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus hic
         et sint eligendi nobis blanditiis nihil commodi ducimus ut, illo quod
