3d82f8a956831d9393cd393207711683e0514ef9
Eduardo Basso <edu_fabric@outlook.com>
2025-10-30 09:01:53 -0300

    A50: Organizando context --modularização--

--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -1,9 +1,8 @@
-import { useTaskContext } from '../../contexts/TaskContext';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import styles from './styles.module.css';
 
 export function CountDown() {
   const { state } = useTaskContext();
-  console.log(state);
 
   return (
     <div className={styles.container}>
